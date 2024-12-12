ebcb03e7aac74f8d0b431b5592e0b8e3c147cac0
LangatD <dorcaschepngetich20@gmail.com>
2024-12-13 01:01:22 +0300

    used sorted function

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+# your code 
+class Anagram():
+    def __init__(self,wordlist):
+        self .wordlist = wordlist.lower()
+    def match(self,word):
+        sorted_word = sorted(self.wordlist)
+        result = []
+        for item in word:
+            if sorted(item.lower()) == sorted(self.wordlist) and item.lower() != self.wordlist:
+                result.append(item)
+        return result
